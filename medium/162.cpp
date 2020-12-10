8b4e6b1047cd0818b7e8c8781342e6c26bee3aa0
kuoteo <894569910@qq.com>
2020-12-10 21:36:51 +0800

    add L162 solution2

--- medium/162.cpp
+++ medium/162.cpp
@@ -13,3 +13,18 @@ public:
         return search(nums, mid + 1, r);
     }
 };
+
+#solution2 迭代 O(logn) O(1) 8ms
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int l = 0, r = nums.size() -1;
+        while(l < r){
+            int mid = (l + r) / 2;
+            if(nums[mid] > nums[mid + 1])
+                r = mid;
+            else
+                l = mid + 1;
+        }
+        return l;
+    }
