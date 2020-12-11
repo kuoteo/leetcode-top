bb595cfa57dd1cd7f7e7b257d7bd83055979967d
kuoteo <894569910@qq.com>
2020-12-11 23:12:22 +0800

    add L189

---
+++ medium/189.cpp
@@ -0,0 +1,17 @@
+#solution1 反转 4ms O(n) O(1)
+class Solution {
+public:
+    void reverse(vector<int>& nums,int start,int end){
+        int temp;
+        while(start<end){
+            temp = nums[start];
+            nums[start++] = nums[end];
+            nums[end--] = temp;
+        }
+    }
+    void rotate(vector<int>& nums, int k) {
+        reverse(nums,0,nums.size()-1);  //反转所有数字
+        reverse(nums,0,k%nums.size()-1);//反转前k个数字
+        reverse(nums,k%nums.size(),nums.size()-1);//反转后n-k个数字
+    }
+};
