1d38738892c9c4cb90fc60edc49e3ae7a8df68a5
kuoteo <894569910@qq.com>
2020-12-11 23:16:39 +0800

    add L238 solution2

--- medium/238.cpp
+++ medium/238.cpp
@@ -17,3 +17,23 @@ public:
         return result;
     }
 };
+
+#solution2 64ms O(n) O(1)
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> result(n);
+        result[0] = 1;
+        for(int i = 1; i < n; ++i){
+            result[i] = nums[i - 1] * result[i - 1];
+        }
+        int R = 1;
+        for(int i = n - 1; i >= 0; --i){
+            //对于索引 i，左边的乘积为 answer[i]，右边的乘积为 R
+            result[i] = result[i] * R;
+            R *= nums[i];
+        }
+        return result;
+    }
+};
