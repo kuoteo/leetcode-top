68708ff63b50a8fa6fad836f3037681a4f4e8356
kuoteo <894569910@qq.com>
2020-12-14 23:06:38 +0800

    add L283 solution2

--- easy/283.cpp
+++ easy/283.cpp
@@ -13,3 +13,19 @@ public:
         }
     }
 };
+
+#solution1 8ms O(n) O(1)
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        if(nums.size()==0) return;
+        int j=0;
+        for(int i = 0;i<nums.size();i++){
+            if(nums[i]!=0){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j++] = temp;
+            }
+        }
+    }
+};
