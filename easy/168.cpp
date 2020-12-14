27aabba3ff3e09932f1ebbf7784b07db78a6dd9d
kuoteo <894569910@qq.com>
2020-12-14 23:02:03 +0800

    add L268 solution2

--- easy/168.cpp
+++ easy/168.cpp
@@ -1,4 +1,4 @@
-#solution1 数学求和 O(n) O(1)
+#solution1 数学求和 44ms O(n) O(1)
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
@@ -6,5 +6,16 @@ public:
         for(int num : nums)
             sum += num;
         return (n * (n + 1)) / 2 - sum;
+    }
+};
+
+#solution1 异或 28ms O(n) O(1)
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int result = nums.size();
+        for(int i = 0; i < nums.size(); ++i)
+            result = result ^ i ^ nums[i];
+        return result;
     }
 };
