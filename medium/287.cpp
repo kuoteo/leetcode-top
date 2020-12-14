13b2b65f767bd81aa81135e60e1c2b4e8622fea5
kuoteo <894569910@qq.com>
2020-12-14 23:10:06 +0800

    add L287 solution2

--- medium/287.cpp
+++ medium/287.cpp
@@ -10,3 +10,26 @@ public:
         return false;
     }
 };
+
+#solution1 二分查找 24ms O(nlogn) O(1)
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        int l = 0, r = n - 1, ans = -1;
+        while(l <= r){
+            int mid = (l + r) / 2;
+            int cnt = 0;
+            for(int i = 0; i < n; ++i){
+                cnt += nums[i] <= mid;
+            }
+            if(cnt <= mid)
+                l = mid + 1;
+            else{
+                r = mid - 1;
+                ans = mid;
+            }
+        }
+        return ans;
+    }
+};
