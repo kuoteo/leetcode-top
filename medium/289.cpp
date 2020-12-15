74b69a6878cfd442fb230f975063dfdc113b59ee
kuoteo <894569910@qq.com>
2020-12-15 21:45:20 +0800

    add L289 solution2

--- medium/289.cpp
+++ medium/289.cpp
@@ -35,3 +35,45 @@ public:
         }
     }
 };
+
+#solution2 0ms O(nm) O(1)
+class Solution {
+public:
+    void gameOfLife(vector<vector<int>>& board) {
+        int neighbors[3] = {-1, 0 , 1}; 
+        int rows = board.size();
+        int cols = board[0].size();
+        
+        for(int row = 0; row < rows; ++row){
+            for(int col = 0; col < cols; ++col){
+                int livenum = 0;
+                for(int i = 0; i < 3; ++i){
+                    for(int j = 0; j < 3; ++j){
+                        if(!(neighbors[i] == 0 && neighbors[j] == 0)){
+                            int r = (neighbors[i] + row);
+                            int c = (neighbors[j] + col);
+
+                            if((r < rows && r >= 0) && (c < cols && c >= 0) && (abs(board[r][c]) == 1)){
+                                ++livenum;
+                            }
+                        }
+                    }
+                }
+                if(board[row][col] == 1 && (livenum < 2 || livenum > 3))
+                    board[row][col] = -1;
+                if(board[row][col] == 0 && livenum == 3)
+                    board[row][col] = 2;
+            }
+        }
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                if (board[row][col] > 0) {
+                    board[row][col] = 1;
+                } else {
+                    board[row][col] = 0;
+                }
+            }
+        }
+
+    }
+};
